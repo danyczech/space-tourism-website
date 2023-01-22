3ad0fa9d1270fd49044ab5fbca3415be402763a5
danakal <dana.kalcikova@centrum.cz>
2023-01-22 15:02:57 +0100

    final fixing
    index, destination and crew pages

--- index.js
+++ index.js
@@ -34,26 +34,26 @@ function changeTabFocus(e) {
   // change the tabindex of the current tab to -1
   if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
     tabs[tabFocus].setAttribute("tabindex", -1)
-  }
 
-  // if the right key is pushed, move to the next tab on the right
+    // if the right key is pushed, move to the next tab on the right
 
-  if (e.keyCode === keydownRight) {
-    tabFocus++
-    if (tabFocus >= tabs.length) {
-      tabFocus = 0
+    if (e.keyCode === keydownRight) {
+      tabFocus++
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0
+      }
     }
-  }
-  // if the left key is pushed, move to the next tab on the left
-  if (e.keyCode === keydownLeft) {
-    tabFocus--
-    if (tabFocus < 0) {
-      tabFocus = tabs.length - 1
+    // if the left key is pushed, move to the next tab on the left
+    if (e.keyCode === keydownLeft) {
+      tabFocus--
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1
+      }
     }
-  }
 
-  tabs[tabFocus].setAttribute("tabIndex", 0)
-  tabs[tabFocus].focus()
+    tabs[tabFocus].setAttribute("tabindex", 0)
+    tabs[tabFocus].focus()
+  }
 }
 
 function changeTabPanel(e) {
@@ -64,15 +64,37 @@ function changeTabPanel(e) {
   const tabContainer = targetTab.parentNode
   const mainContainer = tabContainer.parentNode
 
-  mainContainer
+  tabContainer
+    .querySelector('[aria-selected="true"]')
+    .setAttribute("aria-selected", false)
+  targetTab.setAttribute("aria-selected", "true")
+
+  hideContent(mainContainer, '[role="tabpanel"]')
+
+  /* mainContainer
     .querySelectorAll('[role="tabpanel"]')
-    .forEach((panel) => panel.setAttribute("hidden", true))
+    .forEach((panel) => panel.setAttribute("hidden", true))*/
 
-  mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden")
+  showContent(mainContainer, [`#${targetPanel}`])
+  //mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden")
 
+  hideContent(mainContainer, "picture")
+  /*
   mainContainer
-    .querySelectorAll("picture")
-    .forEach((picture) => picture.setAttribute("hidden", true))
+    .querySelectorAll()
+    .forEach((picture) => picture.setAttribute("hidden", true)) */
 
-  mainContainer.querySelector([`#${targetImage}`]).removeAttribute("hidden")
+  showContent(mainContainer, [`#${targetImage}`])
+
+  // mainContainer.querySelector([`#${targetImage}`]).removeAttribute("hidden")
+}
+
+function hideContent(parent, content) {
+  parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute("hidden", true))
+}
+
+function showContent(parent, content) {
+  parent.querySelector(content).removeAttribute("hidden")
 }
